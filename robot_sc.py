2aaec8cc7b53ba3d40346f45275a57730c9aaca3
Pavel Gesyuk <pageal@gmail.com>
2016-04-11 22:11:44 +0300

    enabled completely

--- robot_sc.py
+++ robot_sc.py
@@ -7,8 +7,6 @@ print("mraa imported")
 
 import time
 
-
-
 #mraa pins
 PWMA=20 #PWM0, which is pin 20 in mraa
 PWMB=14 #PWM1, which is pin 14 in mraa
@@ -18,87 +16,197 @@ STBY=47 #GPIO49, which is pin 47 in mraa
 BIN1=48 #GPIO15, which is pin 48 in mraa
 BIN2=36 #GPIO14, which is pin 36 in mraa
 
-pwma = mraa.Pwm(PWMA)
-pwma.period_us(1000)
-pwma.enable(True)
+class SparkFun_TB6612_Edison():
+    def __init__(self):
+        self._dca = 0.0
+        self._dcb = 0.0
 
-pwmb = mraa.Pwm(PWMB)
-pwmb.period_us(1000)
-pwmb.enable(True)
+        self._pwma = mraa.Pwm(PWMA)
+        self._pwma.period_us(1000)
+        self._pwma.enable(True)
 
-pwma.write(0.01)
-pwmb.write(0.01)
-pwma.write(0.0)
-pwmb.write(0.0)
+        self._pwmb = mraa.Pwm(PWMB)
+        self._pwmb.period_us(1000)
+        self._pwmb.enable(True)
 
-a1 = mraa.Gpio(AIN1)
-a1.dir(mraa.DIR_OUT)
-a1.mode(mraa.MODE_STRONG)
-
-a2 = mraa.Gpio(AIN2)
-a2.dir(mraa.DIR_OUT)
-a1.mode(mraa.MODE_STRONG)
-
-a2 = mraa.Gpio(AIN2)
-a2.dir(mraa.DIR_OUT)
-a2.mode(mraa.MODE_STRONG)
-
-b1 = mraa.Gpio(BIN1)
-b1.dir(mraa.DIR_OUT)
-b1.mode(mraa.MODE_STRONG)
-
-b2 = mraa.Gpio(BIN2)
-b2.dir(mraa.DIR_OUT)
-b2.mode(mraa.MODE_STRONG)
-
-sb = mraa.Gpio(STBY)
-sb.dir(mraa.DIR_OUT)
-sb.mode(mraa.MODE_STRONG)
-sb.write(1)
-
-def move(xpa,xpb,xa1,xa2,xb1,xb2):
-#       time.sleep(0.1)
-        pwma.write(xpa)
-        pwmb.write(xpb)
-        a1.write(xa1)
-        b1.write(xb1)
-        a2.write(xa2)
-        b2.write(xb2)
-#        time.sleep(0.1)
-
-def stop():
-        a1.write(0)
-        b1.write(0)
-        a2.write(0)
-        b2.write(0)
-
-def forward():
-        print("fwd")
-        move(1,1,1,0,1,0)
-
-def back():
-        print("bkw")
-        move(1,1,0,1,0,1)
-
-def lrotate():
-        move(1,1,0,1,1,0)
-
-def rrotate():
-        move(1,1,1,0,0,1)
-
-def lturn():
-        move(0.5,1,1,0,1,0)
-
-def rturn():
-        move(1,0.5,1,0,1,0)
+        self._pwma.write(0.01)
+        self._pwmb.write(0.01)
+        self._pwma.write(0.0)
+        self._pwmb.write(0.0)
 
 
-#stop()
-#time.sleep(5)
-forward()
-time.sleep(5)
-time.sleep(5)
-back()
-time.sleep(5)
-stop()
+        self._a1 = self.init_gpio(AIN1)
+        self._a2 = self.init_gpio(AIN2)
+        self._b1 = self.init_gpio(BIN1)
+        self._b2 = self.init_gpio(BIN2)
+        self._sb = self.init_gpio(STBY)
+
+    def init_gpio(self, pin):
+        pin_obj = mraa.Gpio(pin)
+        pin_obj.dir(mraa.DIR_OUT)
+        pin_obj.mode(mraa.MODE_STRONG)
+        pin_obj.write(1)
+        return pin_obj
+
+
+    def disable_motors(self, disable = True):
+        stby = 0
+        if(disable == False): stby = 1
+        self._sb.write(stby)
+
+    def drive(self, dca, dcb):
+      self._dca=dca;
+      self._dcb=dcb;
+      if (dca < 0):
+        self.rev_a()
+        dca *= -1;
+      else:
+        self.fwd_a()
+
+      if (dcb < 0):
+        self.rev_b()
+        dcb *= -1;
+      else:
+        self.fwd_b()
+    self._pwma.write(dca)
+    self._pwmb.write(dcb)
+
+    def brake(self, brake_a, brake_b):
+      if (brake_a):
+        self._a1.write(1)
+        self._a2.write(1)
+      if (brake_b):
+        self._b1.write(1)
+        self._b2.write(1)
+
+    def fwd_a(self):
+      self._a1.write(0)
+      self._a2.write(1)
+
+    def rev_a(self):
+      self._a1.write(1)
+      self._a2.write(0)
+
+    def fwd_b(self):
+      self._b1.write(0)
+      self._b2.write(1)
+
+    def rev_b(self):
+      self._b1.write(1)
+      self._b2.write(0)
+
+
+    '''
+    bool tb6612::getStandby()
+    {
+      if (mraa_gpio_read(_standbyPin) == 0)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+    void tb6612::getDiffDrive(float* dcA, float* dcB)
+    {
+      *dcA = _dcA;
+      *dcB = _dcB;
+    }
+
+    void tb6612::getShortBrake(bool* brakeA, bool* brakeB)
+    {
+      if ( (mraa_gpio_read(_A1) == 1) && (mraa_gpio_read(_A2) == 1) )
+      {
+        *brakeA = true;
+      }
+      else
+      {
+        *brakeA = false;
+      }
+      if ( (mraa_gpio_read(_B1) == 1) && (mraa_gpio_read(_B2) == 1) )
+      {
+        *brakeB = true;
+      }
+      else
+      {
+        *brakeB = false;
+      }
+    }
+    '''
+
+def test():
+  # The constructor for the tb6612 class object configures all the necessary
+  # pins, exporting them if they aren't already exported, etc. Note that only
+  #  one tb6612 class object may exist at a time, as they share hardware
+  #  resources!
+  motors = SparkFun_TB6612_Edison()
+
+  # The constructor disables the outputs of the tb6612 by asserting the standby
+  #  pin on the controller. You *must* use the disable_motors() function to ena$
+  #  them before proceeding!
+  motors.disable_motors(False)
+
+  # drive() accepts a floating point number for channel A and channel B, in
+  #  the range -1.0 to 1.0 inclusive.
+  print("fwd")
+  motors.drive(1.0,1.0);
+  time.sleep(5);
+
+  print("rev")
+  motors.drive(-1.0,-1.0);
+  time.sleep(5);
+
+  # "short brake" literally means the two outputs are shorted together. This
+  #  drags the motor to a halt in a very short time and then holds it still
+  #  (albeit fairly weakly). The shortBrake() function doesn't change the
+  #  PWM output settings, so when shortBrake() is released by sending a "false"
+  #  parameter, the motor will immediately resume its previous speed.
+  motors.brake(True, True);
+
+  # Return the motors to hi-z state. This also doesn't affect the PWM output,
+  #  so when standby is released (by passing false to this function) the motors
+  #  will immediately resume their former speeds. This also doesn't provide any
+  #  braking, so the motors will coast to a stop much more slowly than with
+  #  shortBrake().
+  motors.disable_motors();
+
+  '''
+  bool brakeA = false;
+  bool brakeB = false;
+  float dcA = 0;
+  float dcB = 0;
+  bool onStandby = false;
+
+  // We've provided a number of "get" functions, to check the current status of
+  //  the device.
+
+  // getStandby() returns true if the motors are on standby, false otherwise.
+  //  This function checks the actual status of the gpio pin used for setting
+  //  the standby mode on the chip, so it will always match reality.
+  onStandby = motors.getStandby();
+
+  // getDiffDrive() checks the *stored* speed value, rather than the current
+  //  value. Thus, if another process alters the PWM output duty cycle without
+  //  actually touching the class object, this may return invalid data. It also
+  //  doesn't return any information about standby or brake status.
+  motors.getDiffDrive(&dcA, &dcB);
+
+  // getShortBrake() checks the pins used for setting the direction/brake mode,
+  //  so the values placed into the pointer parameters by the function are
+  //  accurate at the time the function is called.
+  motors.getShortBrake(&brakeA, &brakeB);
+
+  cout<<"Motor standby status: "<< boolalpha << onStandby << endl;
+  cout<<"Motor A brake status: "<< brakeA << endl;
+  cout<<"Motor B brake status: "<< brakeB << endl;
+  cout<<"Channel A speed: "<< fixed << setprecision(3)<<dcA<<endl;
+  cout<<"Channel B speed: "<<dcB<<endl;
+
+  return 0;
+  '''
+
+test()
+
 
